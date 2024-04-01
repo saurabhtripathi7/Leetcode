1971e5c7791fbf18dcb35103327e7a7d67f1791d
Saurabh Tripathi <saurabhkmtripathi@gmail.com>
2024-04-01 04:55:09 +0000

    [LeetCode Sync] Runtime - 3 ms (2.90%), Memory - 16.5 MB (100.00%)

---
+++ leetcode-solutions/0081-search-in-rotated-sorted-array-ii/solution.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+      int n = nums.size();
+      int s = 0;
+      int e = n-1;
+      while (s <= e)
+      {
+        int mid = s + (e-s)/2;
+        
+        if (nums[mid] == target){
+            return true;
+        }
+        if (nums[s] == nums[mid] && nums[mid] == nums[e]){
+            s++, e--;
+            continue;
+        }
+        // in this approach, we'll not apply BS on whole array, instead we'll trim down the search scope for Bin Search
+        // if left part is sorted
+        if (nums[s] <= nums[mid]){
+            if(nums[s] <= target && target <= nums[mid]){
+                // means element exists in left part
+                e = mid-1;              // left shifting
+            }
+            else { //when element is not present in left sorted part
+                s = mid+1;      // it might be in the right part,  therefore right shifting 
+            }
+        }
+        else{ //if right part is sorted
+            if (nums[mid] <= target && target <= nums[e]){
+                // element is in right half
+                s = mid+1;
+            }
+            else { // not in right half
+                e = mid-1;
+                
+            }
+        }
+
+      }  
+      return false;
+    }
+};
