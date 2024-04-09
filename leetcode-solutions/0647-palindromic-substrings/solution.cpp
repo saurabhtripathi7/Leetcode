bab830b0440e6d02e06a70a1cc89bd410b60d4c1
Saurabh Tripathi <saurabhkmtripathi@gmail.com>
2024-04-09 20:48:26 +0000

    [LeetCode Sync] Runtime - 11 ms (55.30%), Memory - 21.3 MB (26.67%)

---
+++ leetcode-solutions/0647-palindromic-substrings/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int expand(string s, int i, int j){
+        int count = 0;
+        
+        while(i>=0 && j<s.length() && s[i]==s[j]){
+            count++;
+            i--;
+            j++;
+        }
+        return count;
+    }
+    int countSubstrings(string s) {
+        int totalCount=0;
+        for(int center = 0; center < s.length(); center++){
+            // ODD
+            int oddCount= expand(s, center, center);
+            // EVEN
+            int evenCount= expand(s, center, center+1);
+
+            totalCount += oddCount+evenCount;
+        }
+        return totalCount;
+    }
+};
