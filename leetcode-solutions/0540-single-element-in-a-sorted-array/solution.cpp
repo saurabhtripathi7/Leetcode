b28d2ca07d466c20c2f2e8afcf059f6ee5a93885
Saurabh Tripathi <saurabhkmtripathi@gmail.com>
2024-05-04 04:59:48 +0000

    [LeetCode Sync] Runtime - 22 ms (4.18%), Memory - 24.6 MB (100.00%)

---
+++ leetcode-solutions/0540-single-element-in-a-sorted-array/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        int s = 0;
+        int e = n-1;
+        // edge cases
+        if(n==1) return nums[0];
+        if(nums[n-2] != nums[n-1]) return nums[n-1];
+        if(nums[0] != nums[1]) return nums[0];
+
+        
+        while(s <= e){
+            int mid = s + (e-s)/2;
+            if (nums[mid] != nums[mid+1] && nums[mid] != nums[mid-1]){
+                return nums[mid];
+            }
+            if(((mid%2)==0 && nums[mid]==nums[mid+1])
+               ||  ((mid%2)==1 && nums[mid]==nums[mid-1]))
+                s = mid+1;
+            else{
+                e=mid-1;
+            }
+
+        }
+        return -1;
+
+    }
+};
