da60c49617567899b8d61e2433a133bde0b2a952
Saurabh Tripathi <saurabhkmtripathi@gmail.com>
2024-09-21 17:26:38 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.3 MB (85.42%)

--- leetcode-solutions/0496-next-greater-element-i/solution.cpp
+++ leetcode-solutions/0496-next-greater-element-i/solution.cpp
@@ -1,31 +1,47 @@
 class Solution {
 public:
-    
+
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        int mini;
-        vector<int>result;
+        // Stack to store elements from nums2 to find the next greater element
+        stack<int> st;
         
-        for(int i =0; i < nums1.size(); ++i){
-            bool found = false;
-            bool hasGreater = false;
+        // Hash map (unordered_map) to store the next greater element for each number in nums2
+        unordered_map<int, int> mpp;
 
-            // finding nums1[i] in nums2
-            for(int j = 0; j < nums2.size(); ++j)
-            {
-                if(nums1[i] == nums2[j]) found = true;
-            
-                // if found and there's a greater element after it
-                if(found && nums2[j] > nums1[i]){
-                    result.push_back(nums2[j]);
-                    hasGreater = true;
-                    break;
-                }
+        // Traverse nums2 from right to left
+        // This helps maintain a monotonic stack to track the next greater element
+        for(int i = nums2.size() - 1; i >= 0; --i) {
+            int curr = nums2[i];  // Current element in nums2
+
+            // Pop elements from the stack until the top of the stack is greater than the current element
+            // This maintains the decreasing order in the stack
+            while(!st.empty() && st.top() <= curr) {
+                st.pop();
             }
 
-            if(!hasGreater){
-                result.push_back(-1);
+            // If the stack is empty, no greater element exists to the right, so set -1
+            if(st.empty()) {
+                mpp[curr] = -1;
+            } 
+            // Otherwise, the top element is the next greater element
+            else {
+                mpp[curr] = st.top();
             }
+
+            // Push the current element onto the stack for further comparisons
+            st.push(curr);
         }
-        return result;
+
+        // Result vector to store the next greater element for each element in nums1
+        vector<int> res;
+        
+        // For each element in nums1, use the hash map to find its next greater element from nums2
+        for(int i = 0; i < nums1.size(); ++i) {
+            res.push_back(mpp[nums1[i]]);
+        }
+
+        // Return the result vector
+        return res;
     }
 };
+
