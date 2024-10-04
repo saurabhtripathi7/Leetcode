a05722ef2c5d4d1f06655327308061bad2c41742
Saurabh Tripathi <saurabhkmtripathi@gmail.com>
2024-10-04 11:06:15 +0000

    [LeetCode Sync] Runtime - 7 ms (7.13%), Memory - 22.2 MB (100.00%)

--- leetcode-solutions/0026-remove-duplicates-from-sorted-array/solution.cpp
+++ leetcode-solutions/0026-remove-duplicates-from-sorted-array/solution.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
-     int removeDuplicates(vector<int>& nums) {
-        int j = 1;
-        for(int i = 1; i < nums.size(); i++){
-            if(nums[i] != nums[i - 1]){
-                nums[j] = nums[i];
-                j++;
-            }
+    int removeDuplicates(vector<int>& nums) {
+        set<int>val;
+        for(auto i:nums){
+            val.insert(i);
         }
-        return j;
+        nums.clear();
+        for (const auto& elem : val) {
+            nums.push_back(elem);
+        }
+
+        return val.size();
     }
 };
