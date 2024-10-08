cf4cd0f2c0409338a48978806ad9097573d16bb2
Saurabh Tripathi <saurabhkmtripathi@gmail.com>
2024-10-08 19:56:39 +0000

    [LeetCode Sync] Runtime - 94 ms (60.97%), Memory - 70.4 MB (99.88%)

--- leetcode-solutions/0948-sort-an-array/solution.cpp
+++ leetcode-solutions/0948-sort-an-array/solution.cpp
@@ -1,55 +1,7 @@
 class Solution {
 public:
-    void merge(vector<int>& arr, int s, int mid, int e) {
-        int lenLeft = mid - s + 1;
-        int lenRight = e - mid;
-
-        // create left and right arrays for copying
-        vector<int> left(lenLeft);
-        vector<int> right(lenRight);
-
-        // copying values from original array to left and right arrays
-        for (int i = 0; i < lenLeft; ++i) {
-            left[i] = arr[s + i];
-        }
-        for (int i = 0; i < lenRight; ++i) {
-            right[i] = arr[mid + 1 + i];
-        }
-
-        // merge the left and right arrays back into the original array
-        int leftIndex = 0, rightIndex = 0, mainIndex = s;
-
-        while (leftIndex < lenLeft && rightIndex < lenRight) {
-            if (left[leftIndex] <= right[rightIndex]) {
-                arr[mainIndex++] = left[leftIndex++];
-            } else {
-                arr[mainIndex++] = right[rightIndex++];
-            }
-        }
-
-        // copy remaining elements of left array
-        while (leftIndex < lenLeft) {
-            arr[mainIndex++] = left[leftIndex++];
-        }
-
-        // copy remaining elements of right array
-        while (rightIndex < lenRight) {
-            arr[mainIndex++] = right[rightIndex++];
-        }
-    }
-
-    void mergeSort(vector<int>& arr, int s, int e) {
-        if (s >= e) return;
-
-        int mid = s + (e - s) / 2;
-        mergeSort(arr, s, mid);
-        mergeSort(arr, mid + 1, e);
-        merge(arr, s, mid, e);
-    }
-
     vector<int> sortArray(vector<int>& nums) {
-        int size = nums.size();
-        mergeSort(nums, 0, size - 1);
+        sort(nums.begin(), nums.end());
         return nums;
     }
 };
