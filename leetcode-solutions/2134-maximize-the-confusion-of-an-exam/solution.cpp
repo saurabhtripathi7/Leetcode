27883fae225b5cb30eca7f72d20e6689f3190cbc
Saurabh Tripathi <saurabhkmtripathi@gmail.com>
2024-10-09 06:19:16 +0000

    [LeetCode Sync] Runtime - 20 ms (16.53%), Memory - 11.6 MB (100.00%)

---
+++ leetcode-solutions/2134-maximize-the-confusion-of-an-exam/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxConsecutiveAnswers(string ans, int k) {
+        int maxLen = 0;
+        int Fcnt = 0, Tcnt = 0;  // Count of 'F' and 'T'
+        int l = 0;  // Left pointer
+
+        for (int r = 0; r < ans.length(); ++r) {
+            if (ans[r] == 'T') Tcnt++;
+            else if (ans[r] == 'F') Fcnt++;
+
+            int mini = min(Tcnt, Fcnt);  // Minimum of Tcnt and Fcnt in the window
+
+            // If the minimum count exceeds 'k', we need to shrink the window from the left
+            while (mini > k) {
+                if (ans[l] == 'T') Tcnt--;
+                else if (ans[l] == 'F') Fcnt--;
+                l++;  // Shrink the window
+                mini = min(Tcnt, Fcnt);  // Update mini after shrinking the window
+            }
+
+            // Update the maximum length of the valid window
+            maxLen = max(maxLen, r - l + 1);
+        }
+
+        return maxLen;
+    }
+};
