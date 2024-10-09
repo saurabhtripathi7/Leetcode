8b5a633d4910b42b6c15e028579f9f5d83e412c9
Saurabh Tripathi <saurabhkmtripathi@gmail.com>
2024-10-09 19:06:39 +0000

    [LeetCode Sync] Runtime - 49 ms (5.67%), Memory - 38 MB (35.58%)

---
+++ leetcode-solutions/0658-find-k-closest-elements/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        priority_queue<pair<int, int>>maxH;
+        for(int i = 0; i < arr.size(); ++i){
+            maxH.push({abs(arr[i]-x), arr[i]});
+            
+            if(maxH.size() > k){
+                maxH.pop();
+            }
+        }
+        vector<int>res;
+        while(maxH.size() > 0){
+            res.push_back(maxH.top().second);
+            maxH.pop();
+        }
+        sort(res.begin(), res.end());
+        return res;
+
+    }
+};
