69d6ba2c29fed1a28621b03cc0e6221b7fb66fdf
Saurabh Tripathi <saurabhkmtripathi@gmail.com>
2024-11-24 02:46:17 +0000

    [LeetCode Sync] Runtime - 3 ms (39.14%), Memory - 28.9 MB (100.00%)

---
+++ leetcode-solutions/3644-minimum-positive-sum-subarray-/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minimumSumSubarray(vector<int>& nums, int l, int r) {
+        int n = nums.size();
+        int minSum = INT_MAX;
+
+        for (int i = 0; i < n; ++i) {
+            int sum = 0;
+            for (int j = i; j < n; ++j) {
+                sum += nums[j];
+                if (j - i + 1 >= l && j - i + 1 <= r && sum > 0) {
+                    minSum = min(minSum, sum);
+                }
+            }
+        }
+
+        return minSum == INT_MAX ? -1 : minSum;
+    }
+};
