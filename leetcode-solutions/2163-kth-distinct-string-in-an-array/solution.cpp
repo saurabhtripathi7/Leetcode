476a5212dc9b35e1976eef976fd650bcc40dd60d
Saurabh Tripathi <saurabhkmtripathi@gmail.com>
2024-11-26 21:41:53 +0000

    [LeetCode Sync] Runtime - 4 ms (76.91%), Memory - 18.4 MB (56.76%)

---
+++ leetcode-solutions/2163-kth-distinct-string-in-an-array/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string kthDistinct(vector<string>& arr, int k) {
+        unordered_map<string, int> mp;
+        //mp.reserve(arr.size());
+        for(string& s: arr)
+            mp[s]++;
+
+        for(string& s: arr){
+            if (mp[s]==1) k--;
+            if (k==0)  return s;
+        }
+        return "";  
+    }
+};
+
+
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
