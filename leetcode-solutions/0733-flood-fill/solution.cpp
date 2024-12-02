4c5453bc45114ffd899e64b305f1bffc3b2421e8
Saurabh Tripathi <saurabhkmtripathi@gmail.com>
2024-12-02 21:46:35 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.1 MB (55.68%)

---
+++ leetcode-solutions/0733-flood-fill/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void dfs(int row, int col, vector<vector<int>>& ans, vector<vector<int>>& image, int newColor, int delRow[], int delCol[], int initColor){
+        ans[row][col] = newColor;
+
+        int n = image.size();
+        int m = image[0].size();
+
+        for(int i = 0; i < 4; ++i){
+            int nrow = row + delRow[i];
+            int ncol = col + delCol[i];
+
+            if(nrow >= 0 && nrow < n && ncol >= 0 && ncol <m &&
+            image[nrow][ncol] == initColor && ans[nrow][ncol] != newColor){
+                dfs(nrow, ncol, ans, image, newColor, delRow, delCol, initColor);
+            }
+        }        
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int initColor = image[sr][sc];
+        vector<vector<int>>ans = image;
+
+        int delRow[] = {-1, 0, +1, 0};
+        int delCol[] = {0, +1, 0, -1};
+
+        dfs(sr, sc, ans, image, color, delRow, delCol, initColor);
+        return ans;
+    }
+};
