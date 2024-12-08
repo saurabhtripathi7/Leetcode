91b9ff9c53b6357de6ca9589ae68e313731ffe9a
Saurabh Tripathi <saurabhkmtripathi@gmail.com>
2024-12-08 02:27:25 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.5 MB (54.00%)

---
+++ leetcode-solutions/0801-is-graph-bipartite/solution.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool checkBipartiteBFS(int curr, vector<vector<int>>& graph, vector<int>& color, int currColor) {
+        queue<int> q;
+        q.push(curr);
+        color[curr] = currColor;
+
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+            
+            for (int nbr : graph[u]) {
+                if (color[nbr] == color[u]) {
+                    return false; // Same color as parent, not bipartite
+                } else if (color[nbr] == -1) { // Not yet colored
+                    color[nbr] = 1 - color[u]; // Assign the opposite color
+                    q.push(nbr);
+                }
+            }
+        }
+        
+        return true;
+    }
+
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int> color(n, -1); // Initialize all nodes as uncolored (-1)
+        
+        // Iterate over each node in the graph
+        for (int i = 0; i < n; ++i) {
+            if (color[i] == -1) { // If node is not colored yet
+                if (!checkBipartiteBFS(i, graph, color, 1)) {
+                    return false; // If any component is not bipartite
+                }
+            }
+        }
+        
+        return true;
+    }
+};
+
