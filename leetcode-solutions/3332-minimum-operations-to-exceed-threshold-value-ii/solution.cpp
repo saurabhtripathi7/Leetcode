1d30dde158ddaefae334fa3567653afedbbd9f8a
Saurabh Tripathi <saurabhkmtripathi@gmail.com>
2025-02-13 10:43:16 +0000

    [LeetCode Sync] Runtime - 106 ms (16.79%), Memory - 91.5 MB (83.94%)

---
+++ leetcode-solutions/3332-minimum-operations-to-exceed-threshold-value-ii/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        priority_queue<long long, vector<long long>, greater<long long>> pq(nums.begin(), nums.end());
+
+        int cnt = 0;
+        while (pq.top() < k) {
+            cnt++;
+            long long x = pq.top(); 
+            pq.pop();
+            long long y = pq.top(); 
+            pq.pop();
+
+            pq.push(min(x, y) * 2 + max(x, y));
+        }
+        return cnt;
+    }
+};
+
