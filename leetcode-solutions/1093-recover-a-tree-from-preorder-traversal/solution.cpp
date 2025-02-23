050e349363f2e8cd259c7a8c4ad1b9b26a1a95d2
Saurabh Tripathi <saurabhkmtripathi@gmail.com>
2025-02-23 00:04:18 +0000

    [LeetCode Sync] Runtime - 4 ms (45.26%), Memory - 21.5 MB (55.13%)

---
+++ leetcode-solutions/1093-recover-a-tree-from-preorder-traversal/solution.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int n ;
+    TreeNode* solve(string &s, int &i, int depth){
+        if(i >= n) return NULL;
+        int j = i;
+        while(j < n && s[j] == '-'){
+            j++;
+        }
+        int dash = j-i;
+
+        if(dash != depth) return NULL;
+
+        i = j;
+        
+        //construct number
+        int num = 0;
+        while(i < n && isdigit(s[i])){
+            num = num*10 + s[i] - '0';
+            i++;
+        }
+        TreeNode* root = new TreeNode(num);
+
+        root->left = solve(s, i, depth+1);
+        root->right = solve(s, i, depth+1);
+
+        return root;
+
+    }
+    TreeNode* recoverFromPreorder(string traversal) {
+        n = traversal.length();
+        int depth = 0;
+        int i = 0;
+        return solve(traversal, i, 0);
+    }
+};
