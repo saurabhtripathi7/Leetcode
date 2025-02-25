f5b1bf70925e6fb525b10ee2d4d0a4b47318b28b
Saurabh Tripathi <saurabhkmtripathi@gmail.com>
2025-02-25 19:42:06 +0000

    [LeetCode Sync] Runtime - 31 ms (76.56%), Memory - 61.8 MB (91.72%)

--- leetcode-solutions/1014-k-closest-points-to-origin/solution.cpp
+++ leetcode-solutions/1014-k-closest-points-to-origin/solution.cpp
@@ -1,31 +1,12 @@
 class Solution {
 public:
+    static bool cmp(vector<int>& A, vector<int>& B) {
+        return (A[0] * A[0] + A[1] * A[1]) < (B[0] * B[0] + B[1] * B[1]);
+    }
+
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        vector<vector<int>>res;
-        priority_queue<pair<int, pair<int, int>>> maxH;
-        for(int i = 0; i < points.size(); ++i){
-            int x = points[i][0];
-            int y = points[i][1];
-            int D = pow(x, 2) + pow(y, 2);
-            maxH.push({D, {x,y}});
-
-            if(maxH.size() > k){
-                maxH.pop();
-            }
-        }
-
-        while(!maxH.empty()) {
-            res.push_back({maxH.top().second.first, maxH.top().second.second});
-            maxH.pop();
-        }
-
-        return res;
+        sort(points.begin(), points.end(), cmp);
+        return vector<vector<int>>(points.begin(), points.begin() + k);
     }
 };
 
-static const auto fastIO = []() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
-    return 0;
-}();
