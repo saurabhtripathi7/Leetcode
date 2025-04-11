0189e34577df5e088948a18976e9beedf0ffbb0e
Saurabh Tripathi <saurabhkmtripathi@gmail.com>
2025-04-11 07:05:29 +0000

    [LeetCode Sync] Runtime - 4 ms (21.59%), Memory - 77.2 MB (13.10%)

--- leetcode-solutions/0002-add-two-numbers/solution.cpp
+++ leetcode-solutions/0002-add-two-numbers/solution.cpp
@@ -11,56 +11,35 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        // Create a dummy node to act as the starting point of the result list
+       
+        // ListNode* temp1 = l1;
+        // ListNode* temp2 = l2; 
         ListNode* dummy = new ListNode(-1);
-        // `tail` will point to the last node in the result list, initially pointing to dummy
         ListNode* tail = dummy;
-        // Initialize carry to 0. This will keep track of the carry-over value during addition
         int carry = 0;
-
-        // Continue processing as long as there are nodes left in either l1 or l2, or there is a carry
-        while (l1 != NULL || l2 != NULL || carry != 0) {
-            // Initialize val1 and val2 to 0
+    //    we need to continue processing until both l1 and l2 are exhausted and there is no carry left
+    //  if are carry is left then again loop will run to create a node
+        while(l1 != NULL || l2 != NULL || carry != 0){
             int val1 = 0;
-            int val2 = 0;
-
-            // If l1 is not NULL, get the value and move to the next node
-            if (l1 != NULL) {
+            if(l1 != NULL){
                 val1 = l1->val;
                 l1 = l1->next;
-            }
-
-            // If l2 is not NULL, get the value and move to the next node
-            if (l2 != NULL) {
+            } 
+            int val2 = 0;
+            if(l2 != NULL){
                 val2 = l2->val;
                 l2 = l2->next;
-            }
-
-            // Calculate the sum of the two digits and the carry
+            } 
             int sum = carry + val1 + val2;
-            // Determine the digit to store in the current node (units place of sum)
-            int digit = sum % 10;
-            // Update the carry for the next iteration (tens place of sum)
-            carry = sum / 10;
+            int digit = sum%10;
+            carry = sum/10;
 
-            // Create a new node with the digit value
+            // create a newNode at tail to store the digigt
             ListNode* newNode = new ListNode(digit);
-            // Append the new node to the result list
             tail->next = newNode;
-            // Move the tail pointer to the new node
             tail = newNode;
-        }
 
-        // Return the result list, starting from the first node after the dummy
+        }
         return dummy->next;
     }
 };
-
-
-auto init = []() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return 'c';
-}();
-
