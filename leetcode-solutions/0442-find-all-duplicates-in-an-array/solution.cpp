ee6ede6943aebe5b321bd4f89d9ed2d5b040a789
Saurabh Tripathi <saurabhkmtripathi@gmail.com>
2025-04-11 03:50:36 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 48.6 MB (61.31%)

--- leetcode-solutions/0442-find-all-duplicates-in-an-array/solution.cpp
+++ leetcode-solutions/0442-find-all-duplicates-in-an-array/solution.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
-        unordered_map<int,int>freq;
-        for(auto it: nums){
-            freq[it]++;
+        int n = nums.size();
+        vector<int> result;
+        for(int i = 0; i < n; i++) {
+            if(nums[abs(nums[i]) - 1] < 0) {
+                result.push_back(abs(nums[i])); //store if already inverted
+            } else {//invert the num if found first time
+                nums[abs(nums[i]) - 1] = -nums[abs(nums[i]) - 1];
+            }
         }
-        vector<int>res;
-        for(auto pair:freq){
-            if(pair.second == 2) res.push_back(pair.first);
-        }
-        return res;
+        return result;
     }
 };
