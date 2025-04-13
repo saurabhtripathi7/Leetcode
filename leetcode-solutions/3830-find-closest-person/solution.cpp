2bbc5371ccff1e16528795e6f59ce24c26e81b5b
Saurabh Tripathi <saurabhkmtripathi@gmail.com>
2025-04-13 02:33:56 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.5 MB (55.81%)

---
+++ leetcode-solutions/3830-find-closest-person/solution.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int findClosest(int x, int y, int z) {
+        int a = abs(x-z);
+        int b = abs(y-z);
+        if(a == b) return 0;
+        return a < b ? 1 : 2;
+    }
+};
