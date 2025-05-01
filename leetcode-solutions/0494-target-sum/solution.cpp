820783eed781c4ee18d8f99d5e44e96035635c39
Saurabh Tripathi <saurabhkmtripathi@gmail.com>
2025-05-01 03:59:11 +0000

    [LeetCode Sync] Runtime - 11 ms (61.67%), Memory - 25.8 MB (30.01%)

--- leetcode-solutions/0494-target-sum/solution.cpp
+++ leetcode-solutions/0494-target-sum/solution.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
+    int n, offset;
+    int solve(int i, int sum, int target, vector<int>& nums, vector<vector<int>>& dp){
+        if(i >= n) return target == sum;
+        if(dp[i][sum + offset] != -1) return dp[i][sum + offset];
+
+        int pos = solve(i+1, sum + nums[i], target, nums, dp);
+        int neg = solve(i+1, sum - nums[i], target, nums, dp);
+        
+        return dp[i][sum + offset] = pos + neg;
+
+    }
     int findTargetSumWays(vector<int>& nums, int target) {
-        int sum = 0;
-        for (int num : nums) sum += num;
+        n = nums.size();
+        int maxSum = accumulate(nums.begin(), nums.end(), 0);
+        offset = maxSum; // in order to avoid negative indices for sum
+        //sum can range from [-maxSum, +maxSum]
+        // Offset is used to handle negative sums by shifting the range [-maxSum, +maxSum] to [0, 2*maxSum]
+        // This allows us to store all possible sums as valid non-negative indices in the DP table
+        offset = maxSum;
 
-        // If (S + target) is odd or target is not achievable, return 0
-        if ((sum + target) % 2 != 0 || sum < abs(target)) return 0;
-
-        int subsetSum = (sum + target) / 2;
-
-        // DP array to store the number of ways to achieve a sum
-        vector<int> dp(subsetSum + 1, 0);
-        dp[0] = 1; // Base case: one way to make sum 0 (choose no elements)
-
-        // Fill DP table
-        for (int num : nums) {
-            for (int j = subsetSum; j >= num; j--) {
-                dp[j] += dp[j - num];
-            }
-        }
-
-        return dp[subsetSum];
+        vector<vector<int>>dp(n, vector<int>(2*maxSum+1 , -1));
+        
+        return solve(0, 0, target, nums, dp);
     }
 };
-
