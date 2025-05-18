8d1e41a9cfc3cec15670697a2445fc450b247ccc
Saurabh Tripathi <saurabhkmtripathi@gmail.com>
2025-05-18 00:58:00 +0000

    [LeetCode Sync] Runtime - 31 ms (10.65%), Memory - 26.8 MB (70.42%)

--- leetcode-solutions/1170-shortest-common-supersequence-/solution.cpp
+++ leetcode-solutions/1170-shortest-common-supersequence-/solution.cpp
@@ -1,72 +1,55 @@
 class Solution {
 public:
-    string shortestCommonSupersequence(string str1, string str2) {
-        int str1Length = str1.length();
-        int str2Length = str2.length();
-
-        vector<vector<int>> dp(str1Length + 1, vector<int>(str2Length + 1, 0));
-
-        // Initialize the base cases
-        // When str2 is empty, the supersequence is str1 itself
-        // (length = row index)
-        for (int row = 0; row <= str1Length; row++) {
-            dp[row][0] = row;
-        }
-        // When str1 is empty, the supersequence is str2 itself
-        // (length = col index)
-        for (int col = 0; col <= str2Length; col++) {
-            dp[0][col] = col;
-        }
-
-        // Fill the DP table
-        for (int row = 1; row <= str1Length; row++) {
-            for (int col = 1; col <= str2Length; col++) {
-                if (str1[row - 1] == str2[col - 1]) {
-                    // If characters match,
-                    // inherit the length from the diagonal + 1
-                    dp[row][col] = dp[row - 1][col - 1] + 1;
-                } else {
-                    // If characters do not match,
-                    // take the minimum length option + 1
-                    dp[row][col] = min(dp[row - 1][col], dp[row][col - 1]) + 1;
+    string shortestCommonSupersequence(string s1, string s2) {
+        int M = s1.length();
+        int N = s2.length();
+        //dp[m][n] = SCS upto str s1 and s2 of len m, n
+        vector<vector<int>>dp(M+1, vector<int>(N+1, 0));
+        //i, j -> current last char of s1, s2 
+        for(int i = 0; i <= M; ++i){
+            for(int j = 0; j <= N; ++j){
+                if(i == 0 || j == 0){
+                    dp[i][j] = i+j; //rem no of char of non zero str
+                }
+                else if(s1[i-1] == s2[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1]; 
+                }
+                else{
+                    dp[i][j] = 1 + min(dp[i-1][j], dp[i][j-1]);
                 }
             }
         }
 
-        string supersequence = "";
-        int row = str1Length, col = str2Length;
-
-        while (row > 0 && col > 0) {
-            if (str1[row - 1] == str2[col - 1]) {
-                // If characters match, take it from diagonal
-                supersequence += str1[row - 1];
-                row--;
-                col--;
-            } else if (dp[row - 1][col] < dp[row][col - 1]) {
-                // If str1’s character is part of the supersequence, move up
-                supersequence += str1[row - 1];
-                row--;
-            } else {
-                // If str2’s character is part of the supersequence, move left
-                supersequence += str2[col - 1];
-                col--;
+        string res = "";
+        int i = M, j = N;
+        while(i > 0 && j > 0){
+            if(s1[i-1] == s2[j-1]){ // cmp last chars
+                res.push_back(s1[i-1]);
+                i--;
+                j--;                
+            }
+            else if(dp[i-1][j] < dp[i][j-1]){
+                res.push_back(s1[i-1]);
+                i--;
+            }
+            else{
+                res.push_back(s2[j-1]);
+                j--;
             }
         }
 
-        // Append any remaining characters
-        // If there are leftover characters in str1
-        while (row > 0) {
-            supersequence += str1[row - 1];
-            row--;
+        while(i > 0){
+            res.push_back(s1[i-1]);
+            i--;
         }
-        // If there are leftover characters in str2
-        while (col > 0) {
-            supersequence += str2[col - 1];
-            col--;
+        while(j > 0){
+            res.push_back(s2[j-1]);
+            j--;
         }
 
-        // Reverse the built sequence to get the correct order
-        reverse(supersequence.begin(), supersequence.end());
-        return supersequence;
+        reverse(res.begin(), res.end());
+
+        return res;
+
     }
 };
