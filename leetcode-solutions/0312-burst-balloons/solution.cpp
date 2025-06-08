f87d33c75b5a8c3c51d263cc9deaad300018123e
Saurabh Tripathi <saurabhkmtripathi@gmail.com>
2025-06-08 00:11:39 +0000

    [LeetCode Sync] Runtime - 203 ms (33.97%), Memory - 13.8 MB (58.02%)

---
+++ leetcode-solutions/0312-burst-balloons/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+
+    int solve(int i, int j, vector<int>& nums){
+        if(i > j) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int maxi = INT_MIN;
+
+        for(int ind = i; ind <= j; ++ind){
+            int cost = (nums[i-1] * nums[ind] * nums[j+1]) + solve(i, ind-1, nums) + solve(ind+1, j, nums);
+            maxi = max(maxi, cost);
+        }
+
+        return dp[i][j] = maxi;
+    }
+
+    int maxCoins(vector<int>& nums) {
+        int n = nums.size();
+        nums.insert(nums.begin(), 1);
+        nums.push_back(1);
+
+        dp.assign(n + 2, vector<int>(n + 2, -1));
+        return solve(1, n, nums);
+    }
+};
+
