402f9ecf0c90630c36436996474836359259864c
Saurabh Tripathi <saurabhkmtripathi@gmail.com>
2025-06-09 09:19:26 +0000

    [LeetCode Sync] Runtime - 14 ms (20.83%), Memory - 18 MB (71.56%)

---
+++ leetcode-solutions/0085-maximal-rectangle/solution.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    
+    int largestArea(vector<int>& heights){
+         int n = heights.size();
+      stack < int > st;
+      int leftsmall[n], rightsmall[n];
+      for (int i = 0; i < n; i++) {
+        while (!st.empty() && heights[st.top()] >= heights[i]) {
+          st.pop();
+        }
+        if (st.empty())
+          leftsmall[i] = 0;
+        else
+          leftsmall[i] = st.top() + 1;
+        st.push(i);
+      }
+      while (!st.empty())
+        st.pop();
+
+      for (int i = n - 1; i >= 0; i--) {
+        while (!st.empty() && heights[st.top()] >= heights[i])
+          st.pop();
+
+        if (st.empty())
+          rightsmall[i] = n - 1;
+        else
+          rightsmall[i] = st.top() - 1;
+
+        st.push(i);
+      }
+      int maxA = 0;
+      for (int i = 0; i < n; i++) {
+        maxA = max(maxA, heights[i] * (rightsmall[i] - leftsmall[i] + 1));
+      }
+      return maxA;
+    }
+    
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int m=matrix.size();
+        if(m==0) return 0;
+        int n=matrix[0].size(), result=0;
+        vector<int> histogram(n, 0);
+        
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(matrix[i][j]=='1')
+                    histogram[j]+=1;
+                else
+                    histogram[j]=0;
+            }
+            
+            result = max(result, largestArea(histogram));
+        }
+        return result;
+    }
+};
