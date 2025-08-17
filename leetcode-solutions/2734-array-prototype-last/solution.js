d3c6406a0d361072f91314121ee9d67edf245c23
Saurabh Tripathi <saurabhkmtripathi@gmail.com>
2025-08-17 04:19:11 +0000

    [LeetCode Sync] Runtime - 40 ms (80.57%), Memory - 54.5 MB (10.54%)

---
+++ leetcode-solutions/2734-array-prototype-last/solution.js
@@ -0,0 +1,13 @@
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function() {
+    len = this.length;
+    if(len == 0) return -1;
+    return this[len-1];
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
