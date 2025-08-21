eaf5add243df4aa648bb2b3f08e9403a3a7529eb
Saurabh Tripathi <saurabhkmtripathi@gmail.com>
2025-08-21 04:03:56 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 63.5 MB (78.44%)

--- leetcode-solutions/0882-peak-index-in-a-mountain-array/solution.cpp
+++ leetcode-solutions/0882-peak-index-in-a-mountain-array/solution.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    int peakIndexInMountainArray(vector<int>& arr) {
-        int n=arr.size();
-        
+    int peakIndexInMountainArray(vector<int>& nums) {
+        int n= nums.size();
+       if(n==1) return 0;
+       if(nums[0] > nums[1]) return 0;
+       if (nums[n - 1] > nums[n - 2]) return n - 1;
 
-        if (n==1) return 0;
-        if (arr[0] > arr[1]) return 0;
-        if (arr[n-1] > arr[n-2]) return n-1;
-
-        int s=1;
-        int e = n-2;
-
-        while (s <= e)
-        {
+       int s = 1 ;
+       int e = n-2;
+                            
+       while(s<=e)
+       {
             int mid = s + (e-s)/2;
-            if (arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1]) return mid;
-            else if (arr[mid] > arr[mid-1]){
+            if(nums[mid] > nums[mid - 1] && nums[mid] > nums[mid+1]){
+                return mid;
+            }
+            else if(nums[mid] < nums[mid+1]){
                 s = mid+1;
             }
-            else{
+            else if(nums[mid] < nums[mid-1]){
                 e = mid-1;
             }
-        }
-        return -1;
+       } 
+       return -1;
     }
 };
