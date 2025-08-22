9277c11d865a9dcf541a2462921154a81cba7019
Saurabh Tripathi <saurabhkmtripathi@gmail.com>
2025-08-22 04:56:14 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.8 MB (21.11%)

--- leetcode-solutions/1635-number-of-good-pairs/solution.cpp
+++ leetcode-solutions/1635-number-of-good-pairs/solution.cpp
@@ -1,12 +1,20 @@
 class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
         int cnt = 0;
-        for(int i = 0; i < nums.size(); ++i){
-            for(int j = i+1; j < nums.size(); ++j){
-                if(nums[i]==nums[j]) cnt++;
+        int n = nums.size();
+        
+        for (int i = 0; i < n; ) {
+            int j = i;
+            while (j < n && nums[j] == nums[i]) {
+                j++;
             }
+            int k = j - i;  // frequency of nums[i]
+            cnt += (k * (k - 1)) / 2;
+            i = j;  //move to nxt no
         }
         return cnt;
     }
 };
+
