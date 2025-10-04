7ed64bb5be543c5acfdd30de8824e94d3a88f556
Saurabh Tripathi <saurabhkmtripathi@gmail.com>
2025-10-04 10:01:07 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.4 MB (85.99%)

--- leetcode-solutions/0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
+++ leetcode-solutions/0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
@@ -1,48 +1,31 @@
 class Solution {
 public:
+    void bs(int s, int e, vector<int>& nums, int target, vector<int>& ans){
+        if(s > e){
+            return;
+        }
+        int i = s + (e - s) / 2; // mid
+        if(nums[i] == target){
+            ans[0] = min(ans[0], i);
+            ans[1] = max(ans[1], i);
+            // continue search on both sides
+            bs(s, i - 1, nums, target, ans);
+            bs(i + 1, e, nums, target, ans);
+        }
+        else if(nums[i] < target){
+            bs(i + 1, e, nums, target, ans);
+        }
+        else{
+            bs(s, i - 1, nums, target, ans);
+        }
+    }
+
     vector<int> searchRange(vector<int>& nums, int target) {
-        int start = 0;
-        
-        int end = nums.size()-1;
-        
-        vector<int> result = {-1, -1};
-        // finding starting position
-
-        while(start <= end){
-            int mid = start + (end - start)/2;
-
-            if(nums[mid] == target){
-                result[0] = mid;
-                end = mid - 1;
-            }
-            else if(nums[mid] > target){
-                end = mid - 1;
-            }
-            else if(nums[mid] < target){
-                start = mid + 1;
-            }
-        }
-        // reset start and end
-
-        start = 0;
-        end = nums.size()-1;
-
-        // finding last position
-        while (start <= end){
-            int mid = start + (end - start)/2;
-            
-            if(nums[mid] == target){
-                result[1] = mid;
-                start = mid + 1;
-            }
-            else if (nums[mid] > target){
-                end = mid - 1;
-            }
-            else if(nums[mid] < target){
-                start = mid + 1;
-            }
-        }
-
-        return result;
+        vector<int> ans = {INT_MAX, INT_MIN}; // {first, last}
+        bs(0, nums.size() - 1, nums, target, ans);
+        if(ans[0] == INT_MAX) ans[0] = -1;
+        if(ans[1] == INT_MIN) ans[1] = -1;
+        return ans;
     }
 };
+
