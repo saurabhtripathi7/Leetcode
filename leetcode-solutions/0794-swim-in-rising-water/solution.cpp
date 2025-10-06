20c9b91795fe84c278e4f614393b76f25a1fa97b
Saurabh Tripathi <saurabhkmtripathi@gmail.com>
2025-10-06 12:15:24 +0000

    [LeetCode Sync] Runtime - 7 ms (65.56%), Memory - 12.7 MB (92.91%)

--- leetcode-solutions/0794-swim-in-rising-water/solution.cpp
+++ leetcode-solutions/0794-swim-in-rising-water/solution.cpp
@@ -1,47 +1,41 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
-    int swimInWater(vector<vector<int>>& grid) {
-        int n = grid.size();
-        vector<vector<bool>> vis(n, vector<bool>(n, false));
+    typedef pair<int, pair<int, int>> P;
 
-        // Min-Heap to store {time, row, col}
-        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>> pq;
-        
-        // Start from (0, 0) with initial time as grid[0][0]
-        pq.emplace(grid[0][0], 0, 0);
-        vis[0][0] = true;
+    int swimInWater(vector<vector<int>>& grid) 
+    {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        priority_queue<P, vector<P>, greater<P>> pq;
+        vector<vector<int>> vis(m, vector<int>(n, 0));
+        vis[0][0] = 1;
+        pq.push({grid[0][0], {0, 0}});
 
-        // 4-Directional movement (up, right, down, left)
-        int delRow[] = {-1, 0, 1, 0};
-        int delCol[] = {0, 1, 0, -1};
-
-        while (!pq.empty()) {
-            auto [currTime, row, col] = pq.top();
+        while (!pq.empty())
+        {
+            int t = pq.top().first;
+            int row = pq.top().second.first;
+            int col = pq.top().second.second;
             pq.pop();
-            
-            // If we reached the bottom-right cell, return the time
-            if (row == n - 1 && col == n - 1) return currTime;
 
-            // Explore all 4 neighbors
-            for (int i = 0; i < 4; ++i) {
-                int nbrRow = row + delRow[i];
-                int nbrCol = col + delCol[i];
-
-                // Check bounds and unvisited neighbors
-                if (nbrRow >= 0 && nbrRow < n && nbrCol >= 0 && nbrCol < n && !vis[nbrRow][nbrCol]) {
-                    vis[nbrRow][nbrCol] = true;
-                    
-                    // The time needed to reach the neighbor is max of current time and the neighbor's elevation
-                    int newTime = max(currTime, grid[nbrRow][nbrCol]);
-                    pq.emplace(newTime, nbrRow, nbrCol);
+            if (row == m - 1 && col == n - 1)
+            {
+                return t;
+            }
+            for (auto& dir : directions)
+            {
+                int nRow = row + dir.first;
+                int nCol = col + dir.second;
+                if (nRow >= 0 && nRow < m && nCol >= 0 && nCol < n && !vis[nRow][nCol])
+                {
+                    int newTime = max(t, grid[nRow][nCol]);
+                    pq.push({newTime, {nRow, nCol}});
+                    vis[nRow][nCol] = 1;
                 }
             }
         }
-
-        return -1; // Should never reach here
+        return -1;
     }
 };
 
