3d503a0c174d3194c52532afb8f47034c95cb356
Saurabh Tripathi <saurabhkmtripathi@gmail.com>
2025-11-04 04:08:46 +0000

    [LeetCode Sync] Runtime - 32 ms (9.99%), Memory - 40.1 MB (13.25%)

--- leetcode-solutions/3610-find-x-sum-of-all-k-long-subarrays-i/solution.cpp
+++ leetcode-solutions/3610-find-x-sum-of-all-k-long-subarrays-i/solution.cpp
@@ -1,52 +1,29 @@
 class Solution {
 public:
-    vector<int> findXSum(vector<int>& nums, int k, int x) 
-    {
-        int n = nums.size();
-        vector<int> answer;
-        unordered_map<int, int> freq;
-
-        // Initialize frequency map for the first window
-        for (int j = 0; j < k; j++) {
-            freq[nums[j]]++;
+    int xSum(unordered_map<int, int>& freq, int x){
+        vector<pair<int,int>>arr;
+        for(auto& it:freq){
+            int val = it.first;
+            int cnt = it.second;
+            arr.push_back({cnt,val});
         }
-
-        // Calculate x-sum for the first window
-        answer.push_back(calculateXSum(freq, x));
-
-        
-        for (int i = 1; i <= n - k; i++) {
-            // Remove the element going out of the window
-            freq[nums[i - 1]]--;
-            if (freq[nums[i - 1]] == 0) {
-                freq.erase(nums[i - 1]);
-            }
-
-            // Add new element into the window
-            freq[nums[i + k - 1]]++;
-
-            // For the curr window
-            answer.push_back(calculateXSum(freq, x));
-        }
-        
-        return answer;
-    }
-
-private:
-    int calculateXSum(const unordered_map<int, int>& freq, int x) {
-        priority_queue<pair<int, int>> pq; // (freq, value)
-
-        for (const auto& pair : freq) {
-            pq.push({pair.second, pair.first});
-        }
-
+        sort(arr.rbegin(),arr.rend());
         int sum = 0;
-        for (int i = 0; i < x && !pq.empty(); i++) {
-            auto top = pq.top();
-            pq.pop();
-            sum += top.second * top.first; 
+        for(int i = 0;i<min((int)arr.size(),x);i++){
+            sum+=(arr[i].first)*(arr[i].second);
         }
-
         return sum;
+    }
+    vector<int> findXSum(vector<int>& nums, int k, int x) {
+        int n = nums.size();
+        vector<int>ans(n-k+1);
+        for(int i = 0;i<n-k+1;i++){
+            unordered_map<int, int> freq;
+            for(int j = i;j<i+k;j++){
+                freq[nums[j]]++;
+            }
+            ans[i] = xSum(freq,x);
+        }
+        return ans;
     }
 };
