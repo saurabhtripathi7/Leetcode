e50199ff88ebdac917fb80118a0c0caf89c6b200
Saurabh Tripathi <saurabhkmtripathi@gmail.com>
2026-06-29 06:43:14 +0530

    Time: 38 ms (0.63%) | Memory: 9.4 MB (53.16%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    int solve(int i, int j, int m, int n, vector<vector<int>>& dp){
-        if(i == m-1 && j == n-1) return 1;
-        if(i >= m || j >= n) return 0;
-        // cout << " i: "<<i<<" j: "<<j;
-        if(dp[i][j] != -1) return dp[i][j];
-
-        int down = solve(i+1, j, m, n, dp);
-        int right = solve(i, j+1, m, n, dp);
-
-        return dp[i][j] = down + right;
-    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m+1, vector<int>(n+1, -1));
+        vector<vector<int>>dp(m+1, vector<int>(n+1, 0)); // no of ways to reach ith, jth cell
 
-        return solve(0, 0, m, n, dp);
+        for(int i = 0; i < n; ++i) dp[0][i] = 1; // 1st col only 1 way D->D...
+        for(int i = 0; i < m; ++i) dp[i][0] = 1; // 1st row only 1 way R->R...
+
+        for(int i = 1; i < m; ++i){
+            cout<<" i:"<<i;
+            for(int j = 1; j < n; ++j){
+                int up = dp[i-1][j];
+                int left = dp[i][j-1];
+                dp[i][j] = up + left;
+                cout<<" up: "<<up<<" left: "<<left<<" curr: "<<dp[i][j];
+            }
+            cout<<endl;
+        }
+        return dp[m-1][n-1];
     }
 };
