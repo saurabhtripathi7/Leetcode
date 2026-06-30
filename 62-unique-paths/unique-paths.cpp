f5141026fb8618af174eb2f6e4450fb7b0be0a23
Saurabh Tripathi <saurabhkmtripathi@gmail.com>
2026-07-01 03:36:37 +0530

    Time: 2 ms (21.01%) | Memory: 8.3 MB (81.70%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m+1, vector<int>(n+1, 0)); // no of ways to reach ith, jth cell
+        vector<int> prev(n, 0); // for dp[i-1]
 
-        for(int i = 0; i < n; ++i) dp[0][i] = 1; // 1st col only 1 way D->D...
-        for(int i = 0; i < m; ++i) dp[i][0] = 1; // 1st row only 1 way R->R...
-
-        for(int i = 1; i < m; ++i){
-            cout<<" i:"<<i;
-            for(int j = 1; j < n; ++j){
-                int up = dp[i-1][j];
-                int left = dp[i][j-1];
-                dp[i][j] = up + left;
-                cout<<" up: "<<up<<" left: "<<left<<" curr: "<<dp[i][j];
+        for (int i = 0; i < m; ++i) {
+            vector<int> curr(n, 0); // for dp[i]
+            for (int j = 0; j < n; ++j) {
+                if (i == 0 && j == 0) {
+                    curr[j] = 1;
+                    continue;
+                }
+                int up = (i > 0) ? prev[j] : 0;
+                int left = (j > 0) ? curr[j - 1] : 0;
+                curr[j] = up + left;
             }
-            cout<<endl;
+            prev = curr;
         }
-        return dp[m-1][n-1];
+        return prev[n - 1];
     }
 };
