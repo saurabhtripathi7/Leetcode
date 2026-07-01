6ca4aff18c3815af698cee48f89fc9421737b144
Saurabh Tripathi <saurabhkmtripathi@gmail.com>
2026-07-01 10:49:10 +0530

    Time: 54 ms (41.65%) | Memory: 21 MB (27.61%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -1,35 +1,40 @@
 class Solution {
 public:
     int m, n;
-    int solve(int i, int j1, int j2, vector<vector<vector<int>>>& dp, vector<vector<int>>& grid){
-        if(j1 >= n || j2 >= n || j1 < 0 || j2 < 0) return 0;
-        if(i == m-1){
-            if(j1 == j2){
-                return grid[i][j1];
-            }
-            return grid[i][j1] + grid[i][j2];
-        }
-
-        if(dp[i][j1][j2] != -1) return dp[i][j1][j2];
-
-        int curr = (j1 == j2) ? grid[i][j1] : grid[i][j1] + grid[i][j2];
-        // Explore all 9 possbilities
-        int maxi = -1;
-        for(int dj1 = -1; dj1 <= 1; ++dj1){
-            for(int dj2 = -1; dj2 <= 1; ++dj2){
-                int newJ1 = j1 + dj1;
-                int newJ2 = j2 + dj2;
-
-                maxi = max(maxi, solve(i+1, newJ1, newJ2, dp, grid));
-            }
-        }
-        return dp[i][j1][j2] = maxi + curr;
-    }
     int cherryPickup(vector<vector<int>>& grid) {
         m = grid.size();
         n = grid[0].size();
 
-        vector<vector<vector<int>>>dp(m, vector<vector<int>>(n, vector<int>(n, -1)));
-        return solve(0, 0, n-1, dp, grid);
+        vector<vector<vector<int>>>dp(m, vector<vector<int>>(n, vector<int>(n, 0)));
+        
+        // fill for last row
+        for(int j1 = 0; j1 < n; ++j1){
+            for(int j2 = 0; j2 < n; ++j2){
+                if(j1 == j2) dp[m-1][j1][j2] = grid[m-1][j1];
+                else dp[m-1][j1][j2] = grid[m-1][j1] + grid[m-1][j2];
+            }
+        }
+
+        for(int i = m-2; i >= 0; --i){
+            for(int j1 = 0; j1 < n; ++j1){
+                for(int j2 = 0; j2 < n; ++j2){
+                    int curr = (j1 == j2) ? grid[i][j1] : grid[i][j1] + grid[i][j2];
+                    // Explore all 9 possbilities
+                    int maxi = -1;
+                    for(int dj1 = -1; dj1 <= 1; ++dj1){
+                        for(int dj2 = -1; dj2 <= 1; ++dj2){
+                            int newJ1 = j1 + dj1;
+                            int newJ2 = j2 + dj2;
+
+                            if(newJ1 >= 0 && newJ1 < n && newJ2 >= 0 && newJ2 < n){
+                                maxi = max(maxi, dp[i+1][newJ1][newJ2]);
+                            }
+                        }
+                    }
+                    dp[i][j1][j2] = maxi + curr;
+                }
+            }
+        }
+        return dp[0][0][n-1];
     }
 };
