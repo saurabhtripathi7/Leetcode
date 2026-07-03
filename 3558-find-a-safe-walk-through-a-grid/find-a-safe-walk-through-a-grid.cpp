5a5fde4d9380df87fd4134cd4eee92dc403a8148
Saurabh Tripathi <saurabhkmtripathi@gmail.com>
2026-07-03 09:14:54 +0530

    Time: 4 ms (96.23%) | Memory: 32.4 MB (52.04%) - LeetSync

---
+++ 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool findSafeWalk(vector<vector<int>>& grid, int health) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector<vector<int>>dist(m, vector<int>(n, INT_MAX));
+
+        deque<pair<int, int>>dq;
+        dq.emplace_front(0,0); // emplace directly constructs pair obj with passed arguments in deque (no pair creation then copying into deque)
+
+        dist[0][0] = grid[0][0];
+
+        static constexpr int DIRS[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+        while(!dq.empty()){
+            auto [r, c] = dq.front();
+            dq.pop_front();
+
+            if(r == m-1 && c == n-1) return true; // bcz of pruing invalid states before enqueuing them
+
+            for(auto& [dr, dc] : DIRS){
+                int nr = r + dr;
+                int nc = c + dc;
+
+                if(nr < 0 || nc < 0 || nr >= m || nc >= n) continue;
+
+                int cost = dist[r][c] + grid[nr][nc];
+                if(cost >= health) continue; // prune invalid states
+
+                if(cost < dist[nr][nc]){
+                    dist[nr][nc] = cost;
+                    if(grid[nr][nc] == 0) dq.emplace_front(nr, nc);
+                    else dq.emplace_back(nr, nc);
+                }
+            }
+        }
+        return false;
+    }
+};
