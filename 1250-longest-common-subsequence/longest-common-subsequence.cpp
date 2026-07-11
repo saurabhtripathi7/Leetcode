af2f048060cc5a40f50529d9bc0a36744ee4e2fe
Saurabh Tripathi <saurabhkmtripathi@gmail.com>
2026-07-12 04:50:50 +0530

    Time: 0 ms (100.00%) | Memory: 27.8 MB (6.08%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
-    int solve(string &s1, string &s2, vector<vector<int>> &dp){
-        for(int i = s1.size()-1; i>= 0; i--){
-            for(int j = s2.size()-1; j >= 0; j--){
-                if(s1[i] == s2[j]){
-                    dp[i][j] = 1 + dp[i+1][j+1]; 
-                }
-                else{
-                    dp[i][j] = max(dp[i][j+1], dp[i+1][j]);
+    int solve(string& s1, string& s2, vector<vector<int>>& dp) {
+        for (int i = s1.size() - 1; i >= 0; i--) {
+            for (int j = s2.size() - 1; j >= 0; j--) {
+                if (s1[i] == s2[j]) {
+                    dp[i][j] = 1 + dp[i + 1][j + 1];
+                } else {
+                    dp[i][j] = max(dp[i][j + 1], dp[i + 1][j]);
                 }
             }
         }
+        // print LCS
+        int i = 0, j = 0;
+        string ans = "";
+        while (i < s1.size() && j < s2.size()) {
+            if (s1[i] == s2[j]) {
+                ans += s1[i];
+                i++;
+                j++;
+            } else if (dp[i + 1][j] >= dp[i][j + 1]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        cout << ans;
         return dp[0][0];
     }
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.length();
         int m = text2.length();
-        vector<vector<int>>dp(n+1, vector<int>(m+1, 0));
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
         return solve(text1, text2, dp);
     }
 };
@@ -32,8 +46,10 @@ static const auto Saurabh = []() {
 #define Saurabh_HACK
 #ifdef Saurabh_HACK
 const auto __ = []() {
-  struct ___ { static void _() { std::ofstream("display_runtime.txt") << 0 << '\n'; } };
-  std::atexit(&___::_);
-  return 0;
+    struct ___ {
+        static void _() { std::ofstream("display_runtime.txt") << 0 << '\n'; }
+    };
+    std::atexit(&___::_);
+    return 0;
 }();
 #endif
