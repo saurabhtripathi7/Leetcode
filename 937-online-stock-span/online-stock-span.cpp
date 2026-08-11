e22a1700b3601dc5e33d3cb2b3fa621389e1dfe3
Saurabh Tripathi <saurabhkmtripathi@gmail.com>
2026-08-12 00:07:49 +0530

    Time: 19 ms (94.38%) | Memory: 91.9 MB (91.69%) - LeetSync

--- 937-online-stock-span/online-stock-span.cpp
+++ 937-online-stock-span/online-stock-span.cpp
@@ -7,20 +7,11 @@ public:
         st.push({INT_MAX, -1});
     }
     int next(int price) {
-        int ans;
-        if(st.empty())
-        {
-            st.push({price, idx++});
-            return 1;
-        }
-        else{
-            while(!st.empty() && st.top().first <= price){
-                st.pop();
-            }
-            if(st.empty()) return 1;
-            ans = idx - st.top().second;
-            st.push({price, idx++});
-        }
+        while(!st.empty() && st.top().first <= price) st.pop();
+        if(st.empty()) return 1;
+        int ans = idx - st.top().second;
+        st.push({price, idx++});
+            
         return ans;
     }
 };
