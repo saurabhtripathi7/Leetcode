ada865e12bf8e5621c182d946ce82be1d1aa9415
Saurabh Tripathi <saurabhkmtripathi@gmail.com>
2026-09-07 15:09:19 +0530

    Time: 4 ms (76.58%) | Memory: 14.3 MB (70.99%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-    int n;
-    int solve(int idx, int prev, vector<vector<int>>& dp, vector<int>& nums){
-        if(idx == n) return 0;
-
-        if(dp[idx][prev+1] != -1) return dp[idx][prev+1];
-
-        int skip = solve(idx+1, prev, dp, nums);
-        int pick = 0;
-        if (prev == -1 || nums[idx] > nums[prev]) {
-            pick = 1 + solve(idx+1, idx, dp, nums);
-        }
-        return dp[idx][prev+1] = max(pick, skip);
-    }
     int lengthOfLIS(vector<int>& nums) {
-        n = nums.size();
-        vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
+        int n = nums.size();
+        vector<int>sortedAsc;
 
-        return solve(0, -1, dp, nums);
+        for(int i = 0; i < n; ++i){
+            auto iterator = lower_bound(sortedAsc.begin(), sortedAsc.end(), nums[i]);
+
+            if(iterator == sortedAsc.end()) sortedAsc.push_back(nums[i]);
+            else *iterator = nums[i];
+        }
+        return sortedAsc.size();
     }
 };
